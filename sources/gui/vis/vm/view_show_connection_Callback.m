0074343c474d22bf81597ee421225f3e38607b77
reggionh <reggionh@hotmail.com>
2015-11-19 15:35:14 +1300

    Added a new feature: The direction of the connections can now be displayed as an arrow. To use this functionality click on: View -> Show Connection..
    A small quirk of this feature is that the size of the arrowhead needs to change according to the size of the cube to be visually pleasant. This is achieved by using an exponential equation fitted heuristically.

--- sources/gui/vis/vm/view_show_connection_Callback.m
+++ sources/gui/vis/vm/view_show_connection_Callback.m
@@ -4,7 +4,7 @@ global linehandles
 
 dataset=handles.dataset;
 if isempty(dataset.data)
-    msgbox('Please load you dataset!');
+    msgbox('Please load a dataset first');
     return;
 end
 neucube=handles.neucube;
@@ -16,16 +16,16 @@ neumid=neucube.neumid;
 neuinput=neucube.input_mapping{1};
 
 if isempty(neucube_connection)
-    msgbox('Please training the NeuCube!');
+    msgbox('Please train the NeuCube first');
     return;
 end
 
 str=sprintf('Enter the weight threshold to be shown:');
-prompt = {str};
-dlg_title = 'NeuCube';
+prompt = {str,'Display directional connections? (y/n)'};
+dlg_title = 'Show Connection Settings';
 num_lines = 1;
 
-def = {'0.08'};
+def = {'0.08','y'};
 answer = inputdlg(prompt,dlg_title,num_lines,def);
 if isempty(answer)
     return;
@@ -113,12 +113,33 @@ else
                 b = [corrd1(2) corrd2(2)];
                 c = [corrd1(3) corrd2(3)];
                 
+                a_temp = [corrd1(1) corrd1(2) corrd1(3)];
+                b_temp = [corrd2(1) corrd2(2) corrd2(3)];
+                
                 if neucube_weight(i,j) > 0  % the weight of neucube after unsupervised learning using the training data
-                    n=n+1;
-                    linehandles(n)=plot3(a,b,c,'b','linewidth',neucube_weight_scale(i,j));
+                    color = 'b';
+                    %linehandles(n)=plot3(a,b,c,'b','linewidth',neucube_weight_scale(i,j));
+                    %linehandles(n)=arrow3(a_temp,b_temp,'r',1,neucube_weight_scale(i,j));
+                    %arrow3(a_temp,b_temp,'b',12);
                 elseif neucube_weight(i,j) < 0
-                    n=n+1;
-                    linehandles(n)=plot3(a,b,c,'r','linewidth',neucube_weight_scale(i,j));
+                    %n=n+1;
+                    %linehandles(n)=plot3(a,b,c,'r','linewidth',neucube_weight_scale(i,j));
+                    %linehandles(n)=arrow3(a,b,c,'b','linewidth',neucube_weight_scale(i,j));
+                    %arrow3(a_temp,b_temp,'r',12);
+                    color = 'r';
+                end
+                n=n+1;
+                
+                if (strcmp(answer{2},'y') == 1)
+                    param = sprintf('%s%.2f',color,neucube_weight_scale(i,j));
+                    %The size of the arrowhead need to be adjusted
+                    %depending on how big the cube is. The below
+                    %calculation is just a logarithmic function fitted from
+                    %what I think is the appropriate sizes
+                    linewidth = 23*exp(0.00048*number_of_neucube_neural);
+                    arrow3(a_temp,b_temp,param,linewidth);
+                else
+                    plot3(a,b,c,color,'linewidth',neucube_weight_scale(i,j));
                 end
             end
         end
